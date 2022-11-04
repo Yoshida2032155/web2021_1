9fecff89bab924c45d4369838fe83f1485287bd4
龍汰 <104344703+Yoshida2032155@users.noreply.github.com>
2022-11-04 07:02:04 +0000

    トップページとインサートページの作成

---
+++ appVALO.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('test2.db');
+
+app.set('view engine', 'ejs');
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  const message = " ";
+    let sql = "select weapon.id, name, rate, head, body, leg from weapon inner join damage on weapon.id=damage.id";
+    //console.log(sql);    // ②
+    db.serialize( () => {
+        db.all(sql, (error, data) => {
+            if( error ) {
+                res.render('home', {mes:"エラーです"});
+            }
+            //console.log(data);    // ③
+            res.render('selectVALO', {data:data});
+        })
+    })
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send('ページが見つかりません');
+});
+
+app.listen(80, () => console.log("Example app listening on port 80!"));
